438c7fc0151f14f51a43cd1fb5be80f9b45ba47a
Dixit-patel-309 <dixit@hyvikk.com>
2025-07-21 15:16:34 +0530

    url update

--- src/Component/BtnGetStart.jsx
+++ src/Component/BtnGetStart.jsx
@@ -3,32 +3,19 @@ import '../Component/Utility.css'
 
 function BtnGetStart({ category = "default" }) {
 
-  // const handleClick = () => {
-  //   window.open(
-  //     'https://dealers.prepaidiq.com/customer/tfc/finance/application/eyJpdiI6IllxelIvYzhaU0pRYW9RNXFQR0tLRWc9PSIsInZhbHVlIjoiUktYb2hIVWlMNEVldkVsTUNYM1B1Zz09IiwibWFjIjoiYWU1MmZjMGI5YTVkZTg2OWYxMDgyZWMyMjExNzk1OGMxOWUwZWJlZDA4N2M5N2U1MGFkMWI1ZTk0NWJhOTM1NyIsInRhZyI6IiJ9',
-  //     '_blank'
-  //   );
-  // };
 
-  // const handleClick = () => {
-  //   const baseUrl = 'https://dealers.prepaidiq.com/customer/tfc/finance/application/eyJpdiI6IllxelIvYzhaU0pRYW9RNXFQR0tLRWc9PSIsInZhbHVlIjoiUktYb2hIVWlMNEVldkVsTUNYM1B1Zz09IiwibWFjIjoiYWU1MmZjMGI5YTVkZTg2OWYxMDgyZWMyMjExNzk1OGMxOWUwZWJlZDA4N2M5N2U1MGFkMWI1ZTk0NWJhOTM1NyIsInRhZyI6IiJ9';
+  const handleClick = () => {
+    // const baseUrl = 'https://dealers.prepaidiq.com/customer/tfc/finance/application/eyJpdiI6IllxelIvYzhaU0pRYW9RNXFQR0tLRWc9PSIsInZhbHVlIjoiUktYb2hIVWlMNEVldkVsTUNYM1B1Zz09IiwibWFjIjoiYWU1MmZjMGI5YTVkZTg2OWYxMDgyZWMyMjExNzk1OGMxOWUwZWJlZDA4N2M5N2U1MGFkMWI1ZTk0NWJhOTM1NyIsInRhZyI6IiJ9';
+    const baseUrl = 'https://dealers.prepaidiq.com/customer/tfc/finance/application/eyJpdiI6Imd5SHFKdVpzUm1ibFM4eHdINzFHSnc9PSIsInZhbHVlIjoickg4M3JKUzNJQ1FBcnhaZnZBaUZDZz09IiwibWFjIjoiNTA0MmU1MWMwMjEzZTkzMTE2OGEyYjJiNjgwOGE4Y2VmNjY3MjBkMTA4MTBlMzU2ZmExMWQ0YzMwNTE2YTgxMCIsInRhZyI6IiJ9';
 
-  //   const urlWithCategory = `${baseUrl}?category=${encodeURIComponent(category)}`;
+    let urlToOpen = baseUrl;
 
-  //   window.open(urlWithCategory, '_blank');
-  // };
+    if (category && category.trim().toLowerCase() !== 'default') {
+      urlToOpen += `?category=${encodeURIComponent(category.trim())}`;
+    }
 
-const handleClick = () => {
-  const baseUrl = 'https://dealers.prepaidiq.com/customer/tfc/finance/application/eyJpdiI6IllxelIvYzhaU0pRYW9RNXFQR0tLRWc9PSIsInZhbHVlIjoiUktYb2hIVWlMNEVldkVsTUNYM1B1Zz09IiwibWFjIjoiYWU1MmZjMGI5YTVkZTg2OWYxMDgyZWMyMjExNzk1OGMxOWUwZWJlZDA4N2M5N2U1MGFkMWI1ZTk0NWJhOTM1NyIsInRhZyI6IiJ9';
-
-  let urlToOpen = baseUrl;
-
-  if (category && category.trim().toLowerCase() !== 'default') {
-    urlToOpen += `?category=${encodeURIComponent(category.trim())}`;
-  }
-
-  window.open(urlToOpen, '_blank');
-};
+    window.open(urlToOpen, '_blank');
+  };
 
 
   return (

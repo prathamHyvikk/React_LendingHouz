1953c91238f9a34900c120d08057f7c7e0d47f9f
prathamHyvikk <pratham@hyvikk.com>
2026-01-07 12:48:18 +0530

    add redux persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,33 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import personRole from "../features/personRole";
 import authenticate from "../features/authenticate";
 
-export default configureStore({
-  reducer: {
-    person: personRole,
-    auth: authenticate,
-  },
+
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({
+  person: personRole,
+  auth: authenticate,
 });
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["auth", "person"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
